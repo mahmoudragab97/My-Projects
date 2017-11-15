89fb55573bc584cdfea688277cca9b7ef8e94995
Mahmoud ragab shehata <32960286+mahmoudragab97@users.noreply.github.com>
2017-11-15 21:03:01 +0200

    Add files via upload

---
+++ HexaToDecimal/HexaToDecimal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexaToDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            Console.WriteLine("please enter a hexadecimal number");
+            string hexa = Console.ReadLine();
+            int decimalout = int.Parse(hexa, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine(decimalout);
+            Console.ReadLine();
+            goto Start;
+        }
+    }
+}
